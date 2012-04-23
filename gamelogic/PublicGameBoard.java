d2f16c5e524c65f3997d8c784e4a989782470ed8
Allison Hojsak <ahojsak@cs.brown.edu>
2012-04-23 15:05:38 -0400

    added testboard to gameboard

--- gamelogic/PublicGameBoard.java
+++ gamelogic/PublicGameBoard.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class PublicGameBoard {
 	
-	private ArrayList<Vertex>	_points;
+	private ArrayList<Vertex>	_vertices;
 	private ArrayList<Hex>		_hexes;
 	private ArrayList<Edge>		_edges;
 	private ArrayList<Player>	_players;
@@ -16,10 +16,11 @@ public class PublicGameBoard {
 	private int _robberLoc = 1;
 	
 	public PublicGameBoard(ArrayList<Vertex> points, ArrayList<Hex> hexes, ArrayList<Edge> edges, ArrayList<Player> players) {
-		_points = points;
+		_vertices = points;
 		_hexes = hexes;
 		_edges = edges;
 		_players = players;
+		setupTestBoard();
 	}
 	
 	public void setFirstRoundOver() {
@@ -27,8 +28,8 @@ public class PublicGameBoard {
 	}
 	
 	public boolean canBuildSettlement(int p, int v) {
-		if ((_points.get(v).getObject() != 0) || //if point already full
-				(_points.get(v).isBuildable() == false)) { //if not 2 away from other object
+		if ((_vertices.get(v).getObject() != 0) || //if point already full
+				(_vertices.get(v).isBuildable() == false)) { //if not 2 away from other object
 			return false;
 		}
 		if (_firstRound) {
@@ -44,13 +45,13 @@ public class PublicGameBoard {
 	
 	public void buildSettlement(int p, int v) {
 		if (_firstRound) {
-			_players.get(p).addSettlement(_points.get(v));
-			_points.get(v).setObject(1);
-			_points.get(v).setOwner(p);
+			_players.get(p).addSettlement(_vertices.get(v));
+			_vertices.get(v).setObject(1);
+			_vertices.get(v).setOwner(p);
 		}else {
-			_players.get(p).addSettlement(_points.get(v));
-			_points.get(v).setObject(1);
-			_points.get(v).setOwner(p);
+			_players.get(p).addSettlement(_vertices.get(v));
+			_vertices.get(v).setObject(1);
+			_vertices.get(v).setOwner(p);
 			updateLongestRd(p);
 		}
 	}
@@ -60,8 +61,8 @@ public class PublicGameBoard {
 			return false;	
 		}
 		if (_firstRound) {
-			if (_points.get(_edges.get(e).getStartV()).getOwner() == p || 
-					_points.get(_edges.get(e).getEndV()).getOwner() == p) {
+			if (_vertices.get(_edges.get(e).getStartV()).getOwner() == p || 
+					_vertices.get(_edges.get(e).getEndV()).getOwner() == p) {
 				return true;
 			}
 		}
@@ -86,16 +87,16 @@ public class PublicGameBoard {
 	}
 	
 	public boolean canBuildCity(int p, int v) {
-		if (_points.get(v).getObject() != 1 || //if no settlement on vertex
-				!_players.get(p).hasSettlement(_points.get(v))) { //if settlement belongs to player
+		if (_vertices.get(v).getObject() != 1 || //if no settlement on vertex
+				!_players.get(p).hasSettlement(_vertices.get(v))) { //if settlement belongs to player
 				return false;
 		}
 		return true;
 	}
 	
 	public void buildCity(int p, int v) {
-		_players.get(p).addCity(_points.get(v));
-		_points.get(v).setObject(2);
+		_players.get(p).addCity(_vertices.get(v));
+		_vertices.get(v).setObject(2);
 	}
 	
 	public boolean playDevCard(int p, int cardID) {
@@ -122,10 +123,10 @@ public class PublicGameBoard {
 			for (Hex h : _hexes) {
 				if (h.getRollNum() == roll && h.getNum() != _robberLoc) {
 					for (Integer i : h.getVertices()) {
-						int p = _points.get(i).getOwner();
+						int p = _vertices.get(i).getOwner();
 						if (p != -1) {
 							_players.get(p).addCards(new ArrayList(Arrays.asList(h.getResource())));
-							if (_points.get(i).getObject() == 2)  { //if city
+							if (_vertices.get(i).getObject() == 2)  { //if city
 								_players.get(p).addCards(new ArrayList(Arrays.asList(h.getResource())));
 							}
 						}
@@ -152,5 +153,59 @@ public class PublicGameBoard {
 			num = rand.nextInt(_hexes.size() + 1);
 		}
 		_robberLoc = num;
+	}
+	//TODO: send robber info
+	//TODO: getstate method = returns string with # of hexes and each's number
+	
+	
+	
+	
+	
+	
+	public void setupTestBoard() {
+		_vertices = new ArrayList<Vertex>();
+		for (int i = 0; i<13; i++) {
+			_vertices.add(new Vertex(i));
+		}
+		
+		_vertices.get(0).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(11), _vertices.get(1))));
+		_vertices.get(1).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(0), _vertices.get(2), _vertices.get(12))));
+		_vertices.get(2).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(1), _vertices.get(3))));
+		_vertices.get(3).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(2), _vertices.get(4))));
+		_vertices.get(4).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(3), _vertices.get(5))));
+		_vertices.get(5).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(4), _vertices.get(6), _vertices.get(12))));
+		_vertices.get(6).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(5), _vertices.get(7))));
+		_vertices.get(7).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(6), _vertices.get(8))));
+		_vertices.get(8).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(9), _vertices.get(7))));
+		_vertices.get(9).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(8), _vertices.get(10), _vertices.get(12))));
+		_vertices.get(10).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(9), _vertices.get(11))));
+		_vertices.get(11).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(10), _vertices.get(0))));
+		_vertices.get(12).setNeighbors(new ArrayList(Arrays.asList(_vertices.get(1), _vertices.get(5), _vertices.get(9))));
+		
+		_edges = new ArrayList<Edge>();
+		_edges.add(new Edge(11, 0));
+		_edges.add(new Edge(0, 1));
+		_edges.add(new Edge(1, 2));
+		_edges.add(new Edge(2, 3));
+		_edges.add(new Edge(3, 4));
+		_edges.add(new Edge(4, 5));
+		_edges.add(new Edge(5, 6));
+		_edges.add(new Edge(6, 7));
+		_edges.add(new Edge(7, 8));
+		_edges.add(new Edge(8, 9));
+		_edges.add(new Edge(9, 10));
+		_edges.add(new Edge(10, 11));
+		_edges.add(new Edge(9, 12));
+		_edges.add(new Edge(12, 1));
+		_edges.add(new Edge(12, 5));
+		
+		_hexes = new ArrayList<Hex>();
+		_hexes.add(new Hex(0, 3, 0, new ArrayList(Arrays.asList(0,1,12,9,10,11))));
+		_hexes.add(new Hex(1, 4, 1, new ArrayList(Arrays.asList(1,2,3,4,5,12))));
+		_hexes.add(new Hex(2, 5, 2, new ArrayList(Arrays.asList(12,5,6,7,8,9))));
+		
+		_players = new ArrayList<Player>();
+		_players.add(new Player(0));
+		_players.add(new Player(1));
 	}
 }
