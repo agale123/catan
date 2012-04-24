afa19c1c1d8b2050972e5e0d984abb97e6d5e72b
jfedor <jfedor@cs.brown.edu>
2012-04-24 17:04:26 -0400

    code

---
+++ Display/CatanUI/CatanUI/src/catanui/BoardObject.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catanui;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.HashMap;
+
+/**
+ *
+ * @author jfedor
+ */
+public interface BoardObject {
+    
+        
+    public static HashMap<type,Image> images = new HashMap<type,Image>();
+    public enum type {WHEAT,ORE,WOOD,BRICK,SHEEP,DEV,SETTLEMENT,ROAD}
+    
+    public int getW();
+    public int getH();
+    
+    public int getX();
+    public int getY();
+    public void setX(int x);
+    public void setY(int y);
+    
+    public void paint(Graphics g);
+    public void paint(Graphics g, int dx, int dy);
+}
