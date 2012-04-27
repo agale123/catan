230d6b8f963c3398ce4f686d3ccb889edde1521c
Alison Gale <agale@cs.brown.edu>
2012-04-27 14:18:47 -0400

    added script

--- catanui/SplashScreen.java
+++ catanui/SplashScreen.java
@@ -2,6 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
+ 
+ 
+ /**
+  * TODO: Check port number for allowable values
+  */
 
 package catanui;
 
