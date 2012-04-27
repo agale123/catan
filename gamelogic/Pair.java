30f6d368ded03a01c79da3ce31896b690d7e966d
Allison Hojsak <ahojsak@cs.brown.edu>
2012-04-27 13:40:35 -0400

    aaa

---
+++ gamelogic/Pair.java
@@ -0,0 +1,21 @@
+package gamelogic;
+
+public class Pair {
+    
+    Object _a;
+    Object _b;
+    
+    public Pair(Object a, Object b) {
+	_a = a;
+	_b = b;
+    }
+    
+    public Object getA() { return _a;}
+    public Object getB() { return _b; }
+    public void setA(Object o) {
+	_a = o;
+    }
+    public void setB(Object o) {
+	_b = o;
+    }
+}
