e2f403dd73f53e25e1169415fb1aeeabb0be7ce2
Alison Gale <agale@cs.brown.edu>
2012-04-28 03:15:59 -0400

    hi

--- gamelogic/ClientGameBoard.java
+++ gamelogic/ClientGameBoard.java
@@ -147,9 +147,9 @@ public class ClientGameBoard {
 	public void writeBuyRoad(Pair pair) {
 		_client.sendRequest(pair);
 	}
-				System.out.println(h.getVertices().toString());
 
 	public void writeBuildRoad(int vx1, int vy1, int vx2, int vy2) {
+		System.out.println("client trying to build road");
 		_client.sendRequest(1, Integer.toString(_playerNum) + "," + Integer.toString(vx1) + "," + Integer.toString(vy1) + "," + Integer.toString(vx2) + "," + Integer.toString(vy2));
 	}
 	
