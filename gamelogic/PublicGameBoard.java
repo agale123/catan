67a891c27b7e5479baa62231f35607db234034e8
Allison Hojsak <ahojsak@cs.brown.edu>
2012-04-28 04:01:10 -0400

    aaa

--- gamelogic/PublicGameBoard.java
+++ gamelogic/PublicGameBoard.java
@@ -211,7 +211,11 @@ public class PublicGameBoard {
 	
 	/*FIX*/
 	public boolean canBuildRoad(int p, int vx1, int vy1, int vx2, int vy2) {
-		int e = _edgeMap.get(new Pair(new CoordPair(vx1, vy1), new CoordPair(vx2, vy2)));
+		try {
+		    int e = _edgeMap.get(new Pair(new CoordPair(vx1, vy1), new CoordPair(vx2, vy2)));
+		} catch(Exception e) {
+		    return false;
+		} 
 		if (_edges.get(e).hasRoad()) {//if edge already has road 
 			return false;	
 		}
