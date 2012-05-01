f00c24eb31f7381707834c2ab42a5b7f47944a22
Allison Hojsak <ahojsak@cs.brown.edu>
2012-05-01 15:03:51 -0400

    correct numbers added

--- gamelogic/PublicGameBoard.java
+++ gamelogic/PublicGameBoard.java
@@ -51,7 +51,7 @@ public class PublicGameBoard {
 		colSizes = new ArrayList<Integer>(Arrays.asList(3,4,5,4,3));
 		startY = new ArrayList<Integer>(Arrays.asList(3,2,1,2,3));
 		numHexes = 19;
-		numbers = new ArrayList<Integer>(Arrays.asList(11,4,8,12,6,3,6,2,5,11,10,5,10,4,9,2,8,3,6));
+		numbers = new ArrayList<Integer>(Arrays.asList(8,4,11,10,11,3,12,5,9,2,6,9,2,4,5,10,6,3,8));
 	    } else if (numPlayers == 5 || numPlayers == 6) {
 		colSizes = new ArrayList<Integer>(Arrays.asList(4,5,6,7,6,5,4));
 		startY = new ArrayList<Integer>(Arrays.asList(4,3,2,1,2,3,4));
