fc56efecafc6ef763ec23b8191100fae9bc9fad8
Allison Hojsak <ahojsak@cs.brown.edu>
2012-05-01 15:16:38 -0400

    asadasfa

--- gamelogic/Vertex.java
+++ gamelogic/Vertex.java
@@ -4,27 +4,15 @@ import java.util.*;
 import java.lang.*;
 
 public class Vertex {
-
-	private ArrayList<Integer> _neighbors;
-	//private ArrayList<Hex> _hexes;
 	private int	_object = 0;
-	private int _vertexNum;
 	private int _owner = -1;
 	int _x, _y;
+	int _port = -1;
 	
-	/*public Vertex(int num, ArrayList<Integer> neighbors) {
-		_vertexNum = num;
-		_neighbors = neighbors;
-	}*/
 	public Vertex(int x, int y) {
 		_x = x;
 		_y = y;
 	}
-	
-	//public void setNeighbors(ArrayList<Vertex> neighbors) {
-	/*public void setNeighbors(ArrayList<Integer> neighbors) {
-		_neighbors = neighbors;
-	}*/
 	
 	public boolean equals(Object o) {
 	    Vertex v = (Vertex) o;
@@ -35,14 +23,6 @@ public class Vertex {
 		return (int) (_x*3 + _y);
 	}
 	
-	public ArrayList<Integer> getNeighbors() {
-	    return _neighbors;
-	}
-	
-	public int getObject() {
-		return _object;
-	}
-	
 	public void setObject(int x) { //settlement = 1, city = 2
 		_object = x;
 	}
@@ -51,24 +31,16 @@ public class Vertex {
 		_owner = p;
 	}
 	
-	public int getOwner() {
-		return _owner;
+	public void setPort(int i) {
+	    _port = i;
 	}
 	
-	public int getNumber() {
-		return _vertexNum;
-	}
-	
+	public int getObject() { return _object; }
+	public int getOwner() { return _owner; }
+	public int getNumber() { return _vertexNum; }
 	public int getX() { return _x; }
 	public int getY() { return _y; }
-	
-	/*public boolean isBuildable() {
-		for (Vertex v : _neighbors) {
-			if (v.getObject() != 0) {
-				return false;
-			}
-		} return true;
-	}*/
+	public int getPort() { return _port; }
 	
 	public String toString() {
 		return _x + " " + _y + " " + _owner;
