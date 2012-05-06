7d7f6f913dbe552dd42b0dd97b72d6094c66e501
jfedor <jfedor@cs.brown.edu>
2012-05-06 17:50:30 -0400

    asjd

--- catanui/Board.java
+++ catanui/Board.java
@@ -25,22 +25,40 @@ public class Board extends JFrame {
         BoardObject.images.put(BoardObject.type.DEV, Toolkit.getDefaultToolkit().getImage("catanui/devcard.png"));
 	BoardObject.images.put(BoardObject.type.DICE, Toolkit.getDefaultToolkit().getImage("catanui/dice.png"));
 
-		BoardObject.coloredImages.put(BoardObject.type.SETTLEMENT, new Image[]{Toolkit.getDefaultToolkit().getImage("catanui/settlement_blue.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_red.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_green.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_cyan.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_yellow.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_orange.png")});
+	BoardObject.images.put(BoardObject.type.WHEATPORT, Toolkit.getDefaultToolkit().getImage("catanui/wheatport.png"));
+        BoardObject.images.put(BoardObject.type.WOODPORT, Toolkit.getDefaultToolkit().getImage("catanui/woodport.png"));
+        BoardObject.images.put(BoardObject.type.OREPORT, Toolkit.getDefaultToolkit().getImage("catanui/oreport.png"));
+        BoardObject.images.put(BoardObject.type.SHEEPPORT, Toolkit.getDefaultToolkit().getImage("catanui/sheepport.png"));
+        BoardObject.images.put(BoardObject.type.BRICKPORT, Toolkit.getDefaultToolkit().getImage("catanui/brickport.png"));
 
-		BoardObject.coloredImages.put(BoardObject.type.CITY, new Image[]{Toolkit.getDefaultToolkit().getImage("catanui/city_blue.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_red.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_green.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_cyan.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_yellow.png"), Toolkit.getDefaultToolkit().getImage("catanui/city_orange.png")});
+
+	BoardObject.coloredImages.put(BoardObject.type.SETTLEMENT, new Image[]{Toolkit.getDefaultToolkit().getImage("catanui/settlement_blue.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_red.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_green.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_cyan.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_yellow.png"),Toolkit.getDefaultToolkit().getImage("catanui/settlement_orange.png")});
+	BoardObject.coloredImages.put(BoardObject.type.CITY, new Image[]{Toolkit.getDefaultToolkit().getImage("catanui/city_blue.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_red.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_green.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_cyan.png"),Toolkit.getDefaultToolkit().getImage("catanui/city_yellow.png"), Toolkit.getDefaultToolkit().getImage("catanui/city_orange.png")});
        
 	MediaTracker tracker = new MediaTracker(this);
 	tracker.addImage(BoardObject.images.get(BoardObject.type.DICE),0);
+	for (int i=0;i<6;i++) {
+	    tracker.addImage(BoardObject.coloredImages.get(BoardObject.type.SETTLEMENT)[i],0);
+	    tracker.addImage(BoardObject.coloredImages.get(BoardObject.type.CITY)[i],0);
+	}
 	for (BoardObject.type t : BoardObject.cardtypes) {
 		tracker.addImage(BoardObject.images.get(t),0);
 	}
+	for (BoardObject.type t : BoardObject.porttypes) {
+		tracker.addImage(BoardObject.images.get(t),0);
+	}
+
+	SideBar sd = new SideBar(gameLogic);
+	tracker.addImage(sd.tradeGraphic,0);
+	tracker.addImage(sd.buildGraphic,0);
+	tracker.addImage(sd.portsGraphic,0);
 	try {
 		tracker.waitForAll();
 	} catch (InterruptedException ex) {
 		System.out.println("Non-fatal error loading images.");
 	}
 
-	        SideBar sd = new SideBar(gameLogic);
+	        
         sd.setSize(200,700); 
         
         MapPanel mp = new MapPanel(gameLogic);
