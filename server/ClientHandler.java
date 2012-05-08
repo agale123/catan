1d05db936d0bd5a8526943ec3ddda13db4cc8a0e
Alison Gale <agale@cs.brown.edu>
2012-05-07 21:02:52 -0400

    testing removing trades

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -175,6 +175,7 @@ public class ClientHandler extends Thread {
 							break;
 						case 23:
 							// Removing trade
+							System.out.println("Removing all");
 							_pool.broadcast("17/" + details[0], this);
 							break;
 						default:
@@ -196,6 +197,7 @@ public class ClientHandler extends Thread {
 							_pool.broadcastMe(ex, this);
 							_pool.broadcastTo(ex, id);
 							_pool.removeTrade(ex.getTradeID());
+							System.out.println("Removing from all but " + id + " " + _index);
 							_pool.broadcastToElse("17/" + ex.getTradeID(), id, _index);
 						}
 					} else if(ex.isPropose()) {
